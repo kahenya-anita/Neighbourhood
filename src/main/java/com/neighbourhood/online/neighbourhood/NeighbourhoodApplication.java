8d61b29de8dbc5e4c047b2da791baa4fbda1d217
kahenya-anita <anitakahenya1@gmail.com>
2020-11-04 21:56:28 +0300

    Fixed a login bug

--- src/main/java/com/neighbourhood/online/neighbourhood/NeighbourhoodApplication.java
+++ src/main/java/com/neighbourhood/online/neighbourhood/NeighbourhoodApplication.java
@@ -2,8 +2,10 @@ package com.neighbourhood.online.neighbourhood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NeighbourhoodApplication {
 
     public static void main(String[] args) {
